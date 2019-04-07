6433d4459e06afe67b2695a377dc5e8a90a032f1
MaciejApanowicz <maciej.apanowicz@gmail.com>
2019-04-07 23:56:16 +0200

    cleaning comments

--- silentmodetoggle/src/main/java/pl/maciejapanowicz/silentmodetoggle/widget/AppWidget.java
+++ silentmodetoggle/src/main/java/pl/maciejapanowicz/silentmodetoggle/widget/AppWidget.java
@@ -6,10 +6,6 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 
-/**
- * The main class that represents our app's widget.
- * Dispatches to a service to do all of the heavy lifting.
- */
 
 public class AppWidget extends AppWidgetProvider {
 
